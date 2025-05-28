b20fc166340f1e88f95757d14143644db7ad8a05
Ravi Patel <raviprksh77@gmail.com>
2025-05-28 11:52:49 +0530

    feat(styles): integrate Tailwind CSS typography plugin and update global styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="h1-bold">Hello World</h1>
+      <h1 className="h1-bold font-inter">Font Inter</h1>
     </div>
   );
 }
