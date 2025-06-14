33713cdc41a19ab53df5a11b14d420f6b8d6a29f
Ravi Patel <raviprksh77@gmail.com>
2025-06-14 14:49:19 +0530

    feat: add mongoose integration for MongoDB connection handling

---
+++ lib/mongoose.ts
@@ -0,0 +1,51 @@
+/* eslint-disable no-var */
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
+
+interface MongooseCache {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache;
+}
+
+let cached = global.mongoose;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const dbConnect = async (): Promise<Mongoose> => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      dbName: "devFLow",
+    };
+
+    cached.promise = mongoose
+      .connect(MONGODB_URI, opts)
+      .then((mongoose) => {
+        return mongoose;
+      })
+      .catch((error) => {
+        console.error("MongoDB connection error:", error);
+        throw new Error("Failed to connect to MongoDB");
+      });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
+
+export default dbConnect;
